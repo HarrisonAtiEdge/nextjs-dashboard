fac83f17d62d02c47de9ff5f394f2a3bd7c89ba2
AdminIEDGE <151602528+HarrisonAtiEdge@users.noreply.github.com>
2024-11-04 13:28:05 +0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,21 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ['400'],  subsets: ['latin'], });
+
+
+
+
+// .lusitana-regular {
+//     font-family: "Lusitana", serif;
+//     font-weight: 400;
+//     font-style: normal;
+//   }
+  
+//   .lusitana-bold {
+//     font-family: "Lusitana", serif;
+//     font-weight: 700;
+//     font-style: normal;
+//   }
+  
